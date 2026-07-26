7dc669f113d2659508c3f48c023ac98ac61fe5aa
Mike Miller <voidwalker@sagewiresyndicate.com>
2026-07-26 13:15:02 -0400

    Update __init__.py
    Export CRUD modules from package

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,1 +1,25 @@
+"""
+CRUD operations for the SageWire Scale Service.
 
+Each module is responsible for one area of the system:
+
+- scales
+- heartbeats
+- sessions
+- readings
+- events
+"""
+
+from . import events
+from . import heartbeats
+from . import readings
+from . import scales
+from . import sessions
+
+__all__ = [
+    "events",
+    "heartbeats",
+    "readings",
+    "scales",
+    "sessions",
+]
